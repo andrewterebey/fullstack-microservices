aa9814cc362eb958580e2ffc1fd3fed9395d8f9d
andrewterebey <andrew.terebey@gmail.com>
2025-11-21 17:13:13 -0800

    Connected the microservices

---
+++ api-gateway/src/server.ts
@@ -0,0 +1,44 @@
+import fastifyHttpProxy from "@fastify/http-proxy";
+import Fastify from "fastify";
+
+// instance of Fastify
+const app = Fastify({
+    logger: true
+});
+
+
+async function buildServer() {
+    // root route
+    app.get("/", async () => {
+        return { message: "Hello World from API Gateway" };
+    });
+
+    // proxy /users requests to user-service
+    app.register(fastifyHttpProxy, {
+        upstream: 'http://localhost:4001',
+        prefix: '/users',
+        rewritePrefix: '/users'
+    });
+}
+
+// starts the server on port 3000 and host 
+async function startServer() {
+    await buildServer();
+
+    await app.listen({ 
+        port: 3000,
+        host: '0.0.0.0'
+    })
+    console.log("API Gateway is running on port 3000");
+}
+
+// graceful shutdown
+["SIGINT", "SIGTERM"].forEach(signal => {
+    process.on(signal, async () => {
+        console.log(`Recieved ${signal}, closing HTTP server`);
+        await app.close();
+        process.exit(0);
+    });
+});
+
+startServer();
--- user-service/src/server.ts
+++ user-service/src/server.ts
@@ -4,6 +4,18 @@ const app = Fastify({
     logger: true
 });
 
+app.get('/users', async function() {
+    return [
+        {id: 1, name: 'John Doe'},
+        {id: 2, name: 'Jane Smith'}
+    ];
+});
+
+app.get('/users/:id', async function(request) {
+    const { id } = request.params as any;
+    return {id, name: `User ${id}`};
+});
+
 async function start() {
     await app.listen({port: 4001});
     console.log('User Service is running on port 4001');
